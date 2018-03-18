22d7a74977ccfa2965288ed3dba66c1235239ebe
Olivier Riccini <olivier-webdev@hotmail.com>
2018-03-18 19:35:05 -0400

    logo vectorised

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    @project = Project.last
   end
 
   def home
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,99 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-//= require cocoon
-//= require jquery
-//= require bootstrap-sprockets
-import "../components/_main_carousel";
+console.log('Hello World from Webpacker')
 
-console.log('Hello World from Webpacker');
-alert('test');
+
+const path = document.querySelectorAll('path');
+const logo = document.getElementById('logo');
+
+/* Create intervals in order to clear them after action*/
+let interval = null;
+let interval2 = null;
+let interval3 = null;
+let interval4 = null;
+let interval5 = null;
+
+function defInterval() {
+  interval = setInterval(draw, 20);
+}
+
+function defInterval2() {
+  interval2 = setInterval(fill, 50);
+}
+
+function defInterval3() {
+  interval3 = setInterval(changeLogoPositionTop, 9);
+}
+
+function defInterval4() {
+  interval4 = setInterval(changeLogoPositionLeft, 3);
+}
+
+function defInterval5() {
+  interval5 = setInterval(changeLogoWidth, 18);
+}
+
+
+/* Drawing Logo */
+let i = 1500;
+function draw() {
+  i += 5;
+  path.forEach(function(element) {
+    element.style.strokeDasharray = `${i}`;
+    if(i == 3000) {
+      clearInterval(interval);
+    }
+  });
+}
+
+/* Filling Logo */
+let j = 0;
+function fill() {
+  j += 0.01;
+  path.forEach(function(element) {
+    element.style.fill = `rgba(0,0,0,${j}`;
+    if(j == 1) {
+      clearInterval(interval2);
+    }
+  });
+}
+
+var rect = logo.getBoundingClientRect();
+
+let t = rect.top;
+let l = rect.left;
+
+function changeLogoPositionTop() {
+  t -= 1;
+  logo.style.top = `${t}px`;
+  if(t <= 95) {
+    clearInterval(interval3);
+    console.log(t);
+  }
+}
+
+function changeLogoPositionLeft() {
+  l -= 1;
+  logo.style.left = `${l}px`;
+  if(l <= 95) {
+    clearInterval(interval4);
+    console.log(l);
+  }
+}
+
+let w = 500;
+function changeLogoWidth() {
+  w -= 1;
+  logo.style.width = `${w}px`
+  if(w == 350) {
+    clearInterval(interval5);
+  }
+}
+
+
+defInterval();
+setTimeout(defInterval2, 6000);
+setTimeout(defInterval3, 10000);
+setTimeout(defInterval4, 10000);
+setTimeout(defInterval5, 10000);
