e1f264b148804d76ada13432ff7a45201611fbad
Olivier Riccini <olivier-webdev@hotmail.com>
2018-03-29 10:37:10 -0400

    test dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -72,3 +72,18 @@ projects = [
 projects.each do |project|
   Project.create(title: project[:title], type_1: project[:type_1], type_2: project[:type_2], description: project[:description], content: project[:content], state: project[:state])
 end
+
+random_project = (1..10).to_a
+i = -1
+10.times do
+  i += 1
+  new_image = Image.create(project_id: random_project[i])
+  new_image.remote_url_url = "https://picsum.photos/200/300/?random"
+  new_image.save
+end
+
+
+Agency.create(text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+              address: "Biguglia 20620, Lot arbucetta, Lieu dit Ceppe",
+              email: "opb.architecte@gmail.com",
+              phone: "04 95 48 47 22")
